21a62ef7b8e874da0759e20014e593d225881111
Kyle <kyle.brumbaugh9@gmail.com>
2022-12-04 19:51:17 -0500

    Saving deliverable 1

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -26,15 +26,29 @@ order by e.emp_no
 SELECT * from retirement_titles
 
 
+--Copy the query from the Employee_Challenge_starter_code.sql and add it to your Employee_Database_challenge.sql file.
+--Retrieve the employee number, first and last name, and title columns from the Retirement Titles table.
+--These columns will be in the new table that will hold the most recent title of each employee.
+--Use the DISTINCT ON statement to retrieve the first occurrence of the employee number for each set of rows defined by the ON () clause.
+--Exclude those employees that have already left the company by filtering on to_date to keep only those dates that are equal to '9999-01-01'.
+--Create a Unique Titles table using the INTO clause.
+--Sort the Unique Titles table in ascending order by the employee number and descending order by the last date (i.e., to_date) of the most recent title.
 
--- Use Dictinct with Orderby to remove duplicate rows
-SELECT DISTINCT ON (______) _____,
-______,
-______,
-______
+SELECT DISTINCT ON (emp_no) emp_no,
+    first_name,
+    last_name,
+    title
 
-INTO nameyourtable
-FROM _______
-WHERE _______
-ORDER BY _____, _____ DESC;
+INTO unique_titles
+FROM retirement_titles
+WHERE to_date = '9999-01-01'
+ORDER BY emp_no, to_date DESC;
+
+
+
+--Export the Unique Titles table as unique_titles.csv and save it to your Data folder in the Pewlett-Hackard-Analysis folder.
+--Before you export your table, confirm that it looks like this image:
+SELECT * from unique_titles
+
+
 
